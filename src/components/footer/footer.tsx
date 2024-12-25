ba0caa000af62249db6c2ca447c33785d69269e5
laiscrz <lais.albino2003@gmail.com>
2024-12-25 15:22:17 -0300

    feat: updated header and footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <p>&copy; 2024 GitHub Finder</p>
+            <p>&copy; 2024 GitHub Finder. Todos os direitos reservados.</p>
         </footer>
     );
 }
