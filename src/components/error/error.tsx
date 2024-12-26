e7fb9ce4d16df9ac51ef79cd48146453a853e17b
laiscrz <lais.albino2003@gmail.com>
2024-12-26 12:31:42 -0300

    feat: added Error component

---
+++ src/components/error/error.tsx
@@ -0,0 +1,11 @@
+import styles from './error.module.css'
+
+const Error = () =>{
+    return(
+        <div className={styles.error}>
+            <p>Usuário não encontrado !</p>
+        </div>
+    )
+}
+
+export default Error
